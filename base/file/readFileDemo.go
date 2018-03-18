435feff21cae4c1479fad0e6262a3828ee78fa0b
zhangchao <=>
2018-03-18 19:30:06 +0800

    增加数据文件

---
+++ base/file/readFileDemo.go
@@ -0,0 +1,7 @@
+package file
+
+type ReadFileDemo struct{}
+
+func NewReadFileDemo() *ReadFileDemo {
+	return &ReadFileDemo{}
+}
---
+++ base/file/writeFileDemo.go
@@ -0,0 +1,7 @@
+package file
+
+type WriteFileDemo struct{}
+
+func NewWriteFileDemo() *WriteFileDemo {
+	return &WriteFileDemo{}
+}
