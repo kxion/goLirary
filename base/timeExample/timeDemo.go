3c2f2646d99609ac159be1f2d64f9d6dedf59bd6
zhangchao <1033573839@qq.com>
2018-04-16 09:16:20 +0800

    增加时间的相关操作

--- base/timeExample/timeDemo.go
+++ base/timeExample/timeDemo.go
@@ -79,6 +79,28 @@ func (t *TimeDemo) GetMonth(tt time.Time) int64 {
 	return month
 }
 
+func (t *TimeDemo) GetWeek(tt time.Time) int64 {
+	var weekDay int64
+	weekString := tt.Weekday().String()
+	switch weekString {
+	case "Sunday":
+		weekDay = 0
+	case "Monday":
+		weekDay = 1
+	case "Tuesday":
+		weekDay = 2
+	case "Wednesday":
+		weekDay = 3
+	case "Thursday":
+		weekDay = 4
+	case "Friday":
+		weekDay = 5
+	case "Saturday":
+		weekDay = 6
+	}
+	return weekDay
+}
+
 func (t *TimeDemo) GetLastDayOfMonth(year int64, month int64) (string, string) {
 	formatMonthStr := "%d-%d"
 	if month < 10 {
@@ -89,4 +111,57 @@ func (t *TimeDemo) GetLastDayOfMonth(year int64, month int64) (string, string) {
 	firstDay := monthTime.AddDate(0, 0, 0).Format("2006-01-02")
 	endDay := monthTime.AddDate(0, 1, -1).Format("2006-01-02")
 	return firstDay, endDay
+}
+
+func (t *TimeDemo) GetPeriodOfMonthDay(startDate, endDate string) []string {
+	var periodMonth []string
+	startTime, _ := time.Parse("2006-01-02", startDate)
+	endTime, _ := time.Parse("2006-01-02", endDate)
+	startMonthInt := t.GetMonth(startTime)
+	endMonthInt := t.GetMonth(endTime)
+	startYearInt := startTime.Year()
+	endYearInt := endTime.Year()
+	if startYearInt == endYearInt {
+		if startMonthInt == endMonthInt {
+			startDay := startDate
+			endDay := endDate
+			periodMonth = append(periodMonth, startDay, endDay)
+		}
+		for i := startMonthInt; i <= endMonthInt; i++ {
+			startDay, endDay := t.GetLastDayOfMonth(int64(startYearInt), int64(i))
+			if i == startMonthInt {
+				startDay = startDate
+			}
+			if i == endMonthInt {
+				endDay = endDate
+			}
+			periodMonth = append(periodMonth, startDay, endDay)
+		}
+	} else {
+		for i := startYearInt; i <= endYearInt; i++ {
+			if i == startYearInt {
+				for j := startMonthInt; j <= 12; j++ {
+					startDay, endDay := t.GetLastDayOfMonth(int64(startYearInt), int64(j))
+					if j == startMonthInt {
+						startDay = startDate
+					}
+					periodMonth = append(periodMonth, startDay, endDay)
+				}
+			} else if i == endYearInt {
+				for j := 1; j <= int(endMonthInt); j++ {
+					startDay, endDay := t.GetLastDayOfMonth(int64(endYearInt), int64(j))
+					if j == int(endMonthInt) {
+						endDay = endDate
+					}
+					periodMonth = append(periodMonth, startDay, endDay)
+				}
+			} else {
+				for j := 1; j <= 12; j++ {
+					startDay, endDay := t.GetLastDayOfMonth(int64(i), int64(j))
+					periodMonth = append(periodMonth, startDay, endDay)
+				}
+			}
+		}
+	}
+	return periodMonth
 }
--- main.go
+++ main.go
@@ -8,54 +8,8 @@ import (
 
 func main() {
 	t := timeExample.NewTimeDemo()
-	startDate := "2017-05-11"
-	endDate := "2019-11-15"
-	startTime, _ := time.Parse("2006-01-02", startDate)
-	endTime, _ := time.Parse("2006-01-02", endDate)
-	startMonthInt := t.GetMonth(startTime)
-	endMonthInt := t.GetMonth(endTime)
-	startYearInt := startTime.Year()
-	endYearInt := endTime.Year()
-	if startYearInt == endYearInt {
-		if startMonthInt == endMonthInt {
-			startDay := startDate
-			endDay := endDate
-			fmt.Println(startDay, endDay)
-		}
-		for i := startMonthInt; i <= endMonthInt; i++ {
-			startDay, endDay := t.GetLastDayOfMonth(int64(startYearInt), int64(i))
-			if i == startMonthInt {
-				startDay = startDate
-			}
-			if i == endMonthInt {
-				endDay = endDate
-			}
-			fmt.Println(startDay, endDay)
-		}
-	} else {
-		for i := startYearInt; i <= endYearInt; i++ {
-			if i == startYearInt {
-				for j := startMonthInt; j <= 12; j++ {
-					startDay, endDay := t.GetLastDayOfMonth(int64(startYearInt), int64(j))
-					if j == startMonthInt {
-						startDay = startDate
-					}
-					fmt.Println(startDay, endDay)
-				}
-			} else if i == endYearInt {
-				for j := 1; j <= int(endMonthInt); j++ {
-					startDay, endDay := t.GetLastDayOfMonth(int64(endYearInt), int64(j))
-					if j == int(endMonthInt) {
-						endDay = endDate
-					}
-					fmt.Println(startDay, endDay)
-				}
-			} else {
-				for j := 1; j <= 12; j++ {
-					startDay, endDay := t.GetLastDayOfMonth(int64(i), int64(j))
-					fmt.Println(startDay, endDay)
-				}
-			}
-		}
-	}
+	day := "2017-10-15"
+	temp, err := time.Parse("2006-01-02", day)
+	fmt.Println(temp.Weekday().String(), err)
+	fmt.Println(t.GetWeek(temp))
 }
