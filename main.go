6b2cfd48046565d8c547c3c4c3ae7e281bd209c6
zhangchao <1033573839@qq.com>
2019-12-14 15:34:45 +0800

    增加消费代码

--- main.go
+++ main.go
@@ -1,8 +1,21 @@
 package main
 
-import "goLirary/messageTool/kafka"
+import (
+	"goLirary/messageTool/kafka"
+)
 
 func main() {
-	produce := kafka.NewProduce()
-	produce.SendSync([]string{"127.0.0.1:9092"}, "xiaoheiwa", "ceshi", []byte("ceshi123"))
+	openChan := make(chan int)
+	//produce := kafka.NewProduce()
+	cousumer := kafka.NewConsumer()
+	brokers := []string{"127.0.0.1:9092"}
+	topic := "xiaoheiwa"
+	//go func() {
+	//	for i := 0; i < 40; i++ {
+	//		produce.SendSync(brokers, topic, "ceshi", []byte(fmt.Sprintf("test:%d", i)))
+	//		time.Sleep(5 * time.Second)
+	//	}
+	//}()
+	go cousumer.GetTopicMsgs(brokers, topic)
+	<-openChan
 }
---
+++ messageTool/kafka/Consumer.go
@@ -0,0 +1,54 @@
+package kafka
+
+import (
+	"github.com/Shopify/sarama"
+	"log"
+)
+
+type Consumer struct {
+}
+
+func NewConsumer() *Consumer {
+	return new(Consumer)
+}
+
+func (s *Consumer) GetTopicMsgs(brokers []string, topic string) {
+	config := sarama.NewConfig()
+	config.Consumer.Return.Errors = true
+	consumer, err := sarama.NewConsumer(brokers, config)
+	if err != nil {
+		log.Printf("Consumer.CousumerMsg NewConsumer err:%s", err)
+		return
+	}
+	//get all partitionLists from topic
+	partitionList, err := consumer.Partitions(topic)
+	if err != nil {
+		log.Printf("Consumer.CousumerMsg Partitions err:%s ", err)
+		return
+	}
+	for partition := range partitionList {
+		// sarama.OffsetNewest
+		offset := sarama.OffsetNewest
+		if partition == 3 {
+			offset = 1
+		}
+		cousumerPartition, err := consumer.ConsumePartition(topic, int32(partition), offset)
+		if err != nil {
+			log.Printf("Consumer.CousumerMsg ConsumePartition:%d:,err:%s\n", partition, err)
+			return
+		}
+		go s.CousumerMsg(cousumerPartition)
+	}
+}
+
+func (s *Consumer) CousumerMsg(cousumerPartition sarama.PartitionConsumer) {
+	defer cousumerPartition.Close()
+	for {
+		select {
+		case msg := <-cousumerPartition.Messages():
+			log.Println(string(msg.Value), msg.Offset)
+		case err := <-cousumerPartition.Errors():
+			log.Println(err)
+		}
+	}
+}
